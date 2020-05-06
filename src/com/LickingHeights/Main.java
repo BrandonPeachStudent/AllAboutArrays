32c4d8d5decb398ff54652105afed89dc4a5edf4
BRandon <brandon.peach.2022@Student.lhschools.org>
2020-05-06 10:06:09 -0400

    bubbleSort Completed

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -12,16 +12,27 @@ public class Main {
         int[] array = createNumberArray(size, lowestNumber, highestNumber);
 
 
-        Pattern1();
-        Pattern2();
-        Pattern3();
-        Pattern4();
-        Pattern5();
-        System.out.println("The sum of the array is "+sum(array));
-        System.out.println("The minimum of the array is "+minimum(array));
-        System.out.println("The maximum of the array is "+maximum(array));
+       Pattern1();
+       Pattern2();
+       Pattern3();
+       Pattern4();
+       Pattern5();
+       System.out.println("The sum of the array is " + sum(array));
+       System.out.println("The minimum of the array is " + minimum(array));
+       System.out.println("The maximum of the array is " + maximum(array));
+
+        System.out.println("");
+        printArray(array);
+        System.out.println("");
+        System.out.println("After bubble sort");
+        System.out.println("");
+        printArray(bubbleSort(array));
+
+
 
     }
+
+
 
     public static int[] createNumberArray(int size, int lowestNumber, int highestNumber) {
         int[] array = new int[size];
@@ -40,16 +51,18 @@ public class Main {
         }
     }
 
+
     public static int createRandomNumber(int lowestNumber, int highestNumber) {
 
         int number = (int) ((Math.random() * (highestNumber - lowestNumber + 1)) + lowestNumber);
         return number;
     }
-//initial condition, conditional, incrementor/decrementor
-    public static void Pattern1(){
-        for(int rows=0; rows < 6; rows++) {
 
-            for(int columns = 0; columns < 6; columns++){
+    //initial condition, conditional, incrementor/decrementor
+    public static void Pattern1() {
+        for (int rows = 0; rows < 6; rows++) {
+
+            for (int columns = 0; columns < 6; columns++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -57,22 +70,22 @@ public class Main {
         System.out.println();
     }
 
-    public static void Pattern2(){
-      for(int n=1; n < 7; n++){
+    public static void Pattern2() {
+        for (int n = 1; n < 7; n++) {
 
-          for(int row=1; row < 8; row++){
+            for (int row = 1; row < 8; row++) {
 
-              System.out.print(n);
-          }
-          System.out.println();
-      }
-      System.out.println();
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
-    public static void Pattern3(){
-        for(int b=1; b < 7; b++){
+    public static void Pattern3() {
+        for (int b = 1; b < 7; b++) {
 
-            for(int row=1; row<7; row++){
+            for (int row = 1; row < 7; row++) {
 
                 System.out.print(row);
 
@@ -82,11 +95,11 @@ public class Main {
         System.out.println();
     }
 
-    public static void Pattern4(){
+    public static void Pattern4() {
 
-        for(int rows = 1; rows < 7; rows++){
+        for (int rows = 1; rows < 7; rows++) {
 
-            for(int columns = 1; columns <= rows; columns++){
+            for (int columns = 1; columns <= rows; columns++) {
 
                 System.out.print("*");
             }
@@ -95,10 +108,10 @@ public class Main {
         System.out.println();
     }
 
-    public static void Pattern5(){
-        for(int rows = 1; rows <= 6; rows++){
+    public static void Pattern5() {
+        for (int rows = 1; rows <= 6; rows++) {
 
-            for(int columns = 1; columns <= rows; columns++){
+            for (int columns = 1; columns <= rows; columns++) {
                 System.out.print(columns);
             }
             System.out.println();
@@ -106,33 +119,33 @@ public class Main {
         System.out.println();
     }
 
-    public static int sum(int[] array){
+    public static int sum(int[] array) {
         int sum = 0;
 
-        for(int i=0; i<array.length; i++){
-           sum += array[i];
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
 
         }
 
         return sum;
     }
 
-    public static int minimum(int[] array){
+    public static int minimum(int[] array) {
         int minimum = array[0];
 
-        for(int i = 1; i < array.length; i++){
-            if(minimum > array[i]) {
+        for (int i = 1; i < array.length; i++) {
+            if (minimum > array[i]) {
                 minimum = array[i];
             }
         }
         return minimum;
     }
 
-    public static int maximum(int[] array){
+    public static int maximum(int[] array) {
         int maximum = array[0];
 
-        for(int i = 1; i < array.length; i++){
-            if(maximum < array[i]){
+        for (int i = 1; i < array.length; i++) {
+            if (maximum < array[i]) {
                 maximum = array[i];
 
             }
@@ -141,5 +154,20 @@ public class Main {
     }
 
 
+    public static int[] bubbleSort(int[] array) {
+
+        for(int i = 0; i < array.length - 1; i++){
+            for(int j = i + 1; j < array.length; j++){
+                if (array[i] > array[j]){
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        return array;
+    }
 
 }
+
+
